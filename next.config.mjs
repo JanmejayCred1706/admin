28c8b2b6471b2a1151966221c75afe492173961f
toffee17 <janmejay.pandey@garantie.in>
2024-08-09 12:27:23 +0530

    ---

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,15 @@ const nextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    // ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true,
   },
-  // typescript: {
-  //   // !! WARN !!
-  //   // Dangerously allow production builds to successfully complete even if
-  //   // your project has type errors.
-  //   // !! WARN !!
-  //   ignoreBuildErrors: true,
-  // },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   productionBrowserSourceMaps: true,
   // sentry: { hideSourceMaps: true },
   // async redirects() {
